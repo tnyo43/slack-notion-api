c2348dff2400f3b4cf1fbc9d98e016f650d99081
tnyo43 <tnyo43@gmail.com>
2021-09-14 09:29:36 +0900

    console.log を追加

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 const urlVerificationType = "url_verification";
 
 export default (req: VercelRequest, res: VercelResponse) => {
+  console.log(req.body);
   if (!!req.body && req.body.type === urlVerificationType) {
     res.send({
       text: "Hello SlackApp!",
