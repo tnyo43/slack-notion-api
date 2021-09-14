e3ff7b3fb365806ff20b61ee838ad51889d63cf1
tnyo43 <tnyo43@gmail.com>
2021-09-14 09:20:54 +0900

    request の body が undefined のときの対応

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 const urlVerificationType = "url_verification";
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  if (req.body.type === urlVerificationType) {
+  if (!!req.body && req.body.type === urlVerificationType) {
     res.send({
       text: "Hello SlackApp!",
       challenge: req.body.challenge,
