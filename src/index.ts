acf553fb62d8ca09d9584773cc3177209e03d928
tnyo43 <tnyo43@gmail.com>
2021-09-18 23:45:11 +0900

    api client の class を導入

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { env } from "./constants/env";
-import { post } from "./libs/notion/api";
+import { debug } from "./libs/debug";
+import { getApiClient } from "./libs/notion/api";
 
 const main = async () => {
-  await post(env.NOTION_TEST_DB_ID, env.NOTION_INTEGRATION_INTERNAL_TOKEN);
-  console.log("Hello World");
+  try {
+    const result = await getApiClient({
+      databaseId: env.NOTION_TEST_DB_ID,
+      token: env.NOTION_INTEGRATION_INTERNAL_TOKEN,
+    }).post();
+    console.log(result.status);
+  } catch (error: any) {
+    debug("post", "error", error.response.data);
+  }
 };
 
 main();
--- src/libs/notion/api.ts
+++ src/libs/notion/api.ts
@@ -1,26 +1,61 @@
 import axios from "axios";
 
-export const post = async (databaseId: string, notionToken: string) =>
-  await axios({
-    method: "post",
-    url: `https://api.notion.com/v1/pages`,
-    headers: {
-      Authorization: `Bearer ${notionToken}`,
-      "Notion-Version": "2021-05-13",
-      "Content-Type": "application/json",
-    },
-    data: {
-      parent: { database_id: databaseId },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: "連休の予定",
+type ApiClientKey = { databaseId: string; token: string };
+
+interface IApiClient {
+  post: () => Promise<any>;
+}
+
+class ApiClient implements IApiClient {
+  private databaseId: string;
+  private token: string;
+
+  constructor(key: ApiClientKey) {
+    this.databaseId = key.databaseId;
+    this.token = key.token;
+  }
+
+  async post() {
+    return axios.post<any>(
+      "https://api.notion.com/v1/pages",
+      {
+        parent: { database_id: this.databaseId },
+        properties: {
+          Name: {
+            title: [
+              {
+                text: {
+                  content: "連休の予定",
+                },
               },
-            },
-          ],
+            ],
+          },
         },
       },
-    },
-  });
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Notion-Version": "2021-05-13",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
+
+class ApiClientFactory {
+  private static apis = new Map<string, ApiClient>();
+
+  static getApiClientInstance(key: ApiClientKey) {
+    const keyText = key.databaseId + key.token; // Hash のような役割
+
+    const client = ApiClientFactory.apis.get(keyText);
+    if (client) return client;
+
+    const newClient = new ApiClient(key);
+    ApiClientFactory.apis.set(keyText, newClient);
+    return newClient;
+  }
+}
+
+export const getApiClient = ApiClientFactory.getApiClientInstance;
